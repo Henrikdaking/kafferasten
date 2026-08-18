1e7c9045a187b4fa52f74a8b52f7a8c766e52bc7
Henrikdaking <carlssonhenrik@yahoo.se>
2026-08-18 16:50:27 +0200

    Update get-trends.js

--- netlify/functions/get-trends.js
+++ netlify/functions/get-trends.js
@@ -24,7 +24,7 @@ Returnera SVARET ENBART som giltig JSON utan markdown-formatering med följande 
   "sourcesHtml": "<a href='https://sverigesradio.se' target='_blank'>Sveriges Radio</a> • <a href='https://aftonbladet.se' target='_blank'>Aftonbladet</a>"
 }`;
 
-    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`, {
+   const response = await fetch(`https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
